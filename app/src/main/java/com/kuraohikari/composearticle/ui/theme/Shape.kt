acf3d04a085a49b6bfe9949c806ec305ec4e25cc
KuraoHikari <dewaindra705@gmail.com>
2023-10-09 19:09:08 +0800

    init

---
+++ app/src/main/java/com/kuraohikari/composearticle/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.kuraohikari.composearticle.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
